ad47028394abbf5f3833100334ffe8d5e3be03c2
jmortada <158744391+jmortada@users.noreply.github.com>
2025-04-17 21:56:24 +0300

    modified the hover trail to be multiple colored

--- eas.js
+++ eas.js
@@ -1,10 +1,12 @@
+const color = ["#00ff00", "#ff0000", "#0000ff", "#ffff00", "#ff00ff", "#00ffff"];
+
 const cont = document.querySelector(".container");
 for(let i =0; i<256;i++){
     let div = document.createElement("div");
     div.classList.add("box");
     div.setAttribute("style", "width:" + (cont.clientWidth/16) + "px; height:" + (cont.clientWidth/16) + "px;");
     div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "#00ff00";
+        div.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
     });
     cont.appendChild(div);
 }
@@ -20,7 +22,7 @@ nbox.addEventListener("click", () => {
         let div = document.createElement("div");
         div.setAttribute("style", "width:" + (cont.clientWidth/nsquare) + "px; height:" + (cont.clientWidth/nsquare) + "px;");
         div.addEventListener("mouseover", () => {
-            div.style.backgroundColor = "#00ff00";
+            div.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
         });
         cont.appendChild(div);
 

623544de890688b7384854b6006ba9dd15989f33
jmortada <158744391+jmortada@users.noreply.github.com>
2025-04-17 21:40:39 +0300

    added an event that lets user decide number of grids to play on

--- eas.js
+++ eas.js
@@ -8,3 +8,22 @@ for(let i =0; i<256;i++){
     });
     cont.appendChild(div);
 }
+
+let nbox = document.querySelector(".btn");
+nbox.addEventListener("click", () => {
+    let nsquare = "";
+    do {
+        nsquare = prompt("Enter the number of squares per side");
+    } while (nsquare >100);
+    cont.innerHTML = "";
+    for (let i =0; i<nsquare*nsquare;i++){
+        let div = document.createElement("div");
+        div.setAttribute("style", "width:" + (cont.clientWidth/nsquare) + "px; height:" + (cont.clientWidth/nsquare) + "px;");
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "#00ff00";
+        });
+        cont.appendChild(div);
+
+    }
+}
+);

5a9e4af15af44704bdbf421242121fe0165e8703
jmortada <158744391+jmortada@users.noreply.github.com>
2025-04-17 18:03:34 +0300

    an evemt that leaves a green trail after mouse hovers over div

--- eas.js
+++ eas.js
@@ -1,7 +1,10 @@
 const cont = document.querySelector(".container");
 for(let i =0; i<256;i++){
-    const div = document.createElement("div");
+    let div = document.createElement("div");
     div.classList.add("box");
     div.setAttribute("style", "width:" + (cont.clientWidth/16) + "px; height:" + (cont.clientWidth/16) + "px;");
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "#00ff00";
+    });
     cont.appendChild(div);
 }
